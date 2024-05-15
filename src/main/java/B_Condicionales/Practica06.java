d74939b4dc398a7dbb973e4124b01038beb263c9
DAWTarde <DAWTarde@AEMCENT-PN6GCRF>
2024-05-15 16:36:43 +0200

    subiendo practicas

--- src/main/java/B_Condicionales/Practica06.java
+++ src/main/java/B_Condicionales/Practica06.java
@@ -1,14 +1,31 @@
 /*  Practica 06:
-    Hacer un programaque tome dos números y diga si ambos son pares o impares.
+    Hacer un programa que tome dos números y diga si ambos son pares o impares.
  */
 package B_Condicionales;
 
+import java.util.Scanner;
+
 /**
- *
  * @author Ahmed Darhbane
  */
 public class Practica06 {
+
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        int num1, num2;
+
+        System.out.print("Digite el PRIMER numero: ");
+        num1 = sc.nextInt();
+        System.out.print("Digite el SEGUNDO numero: ");
+        num2 = sc.nextInt();
+
+        if (num1%2==0 && num2%2==0) {
+            System.out.println("Ambos numeros son PARES");
+        } else if (num1%2==0 || num2%2==0) {
+            System.out.println("Uno es PAR y el otro es IMPAR");
+        } else {
+            System.out.println("Los dos son IMPARES");
+        }
+
     }
 }
--- src/main/java/B_Condicionales/Practica07.java
+++ src/main/java/B_Condicionales/Practica07.java
@@ -1,13 +1,46 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+/*  Practica 07: 
+    Pedir tres números y mostrarlos ordenados de mayor a menor.
  */
 package B_Condicionales;
 
+import java.util.Scanner;
+
 /**
- *
- * @author Ahmed
+ * @author Ahmed Darhbane
  */
 public class Practica07 {
-    
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double num1, num2, num3;
+        boolean flag = false;
+
+        System.out.print("Digite el PRIMER numero: ");
+        num1 = sc.nextDouble();
+        System.out.print("Digite el SEGUNDO numero: ");
+        num2 = sc.nextDouble();
+        System.out.print("Digite el TERCER numero: ");
+        num3 = sc.nextDouble();
+
+        if (num1 >= num2 && num1 >= num3) {
+            if (num2 >= num3) {
+                System.out.println(num1 + " > " + num2 + " > " + num3);
+            } else {
+                System.out.println(num1 + " > " + num3 + " > " + num2);
+            }
+        } else if (num2 >= num1 && num2 >= num3) {
+            if (num1 >= num3) {
+                System.out.println(num2 + " > " + num1 + " > " + num3);
+            } else {
+                System.out.println(num2 + " > " + num3 + " > " + num1);
+            }
+        } else {
+            if (num1 >= num2) {
+                System.out.println(num3 + " > " + num1 + " > " + num2);
+            } else {
+                System.out.println(num3 + " > " + num2 + " > " + num1);
+            }
+        }
+
+    }
 }
