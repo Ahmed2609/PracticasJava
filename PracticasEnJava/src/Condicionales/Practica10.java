3004a404bf5708c03bffdfece85a47ac5afc243f
DAWTarde <DAWTarde@AEMCENT-PN6GCRF>
2024-05-15 17:01:06 +0200

    subiendo practicas

---
+++ PracticasEnJava/src/Condicionales/Practica10.java
@@ -0,0 +1,14 @@
+package Condicionales;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author DAWTarde
+ */
+public class Practica10 {
+    
+}
