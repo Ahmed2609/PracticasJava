067e4f57df5001da17b8ea6bba277d52a949f39f
Ahmed Dar <darhbaneahmed@gmail.com>
2024-05-15 12:49:19 +0200

    Subiendo

--- src/main/java/B_Condicionales/Practica02.java
+++ src/main/java/B_Condicionales/Practica02.java
@@ -1,15 +1,32 @@
-/*
-
+/*  Practica 02:
+    Pedir dos números y decir cual es el mayor o si son iguales.
  */
 package B_Condicionales;
+
+import java.util.Scanner;
 
 /**
  *
  * @author Ahmed Darhbane
  */
 public class Practica02 {
+
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        double num1, num2;
+
+        System.out.print("Digite el PRIMER numero: ");
+        num1 = sc.nextDouble();
+        System.out.print("Digite el SEGUNDO numero: ");
+        num2 = sc.nextDouble();
+
+        if (num1 > num2) {
+            System.out.println(num1 + " ES mayor que " + num2);
+        } else if (num2 > num1) {
+            System.out.println(num2 + " ES mayor que " + num1);
+        } else {
+            System.out.println("Los numeros son iguales.");
+        }
     }
-    
+
 }
---
+++ src/main/java/B_Condicionales/Practica03.java
@@ -0,0 +1,14 @@
+/*  Practica 03:
+    Hacer un programa que lea un carácter por teclado compruebe si es una letra mayúscula.
+ */
+package B_Condicionales;
+
+/**ç
+ * @author Ahmed
+ */
+public class Practica03 {
+    public static void main(String[] args) {
+        
+    }
+    
+}
