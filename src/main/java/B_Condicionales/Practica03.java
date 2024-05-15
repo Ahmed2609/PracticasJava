d3c5a6f751221253be98620122c7942fb891ab2b
Ahmed Dar <darhbaneahmed@gmail.com>
2024-05-15 13:53:36 +0200

    Subiendo

--- src/main/java/B_Condicionales/Practica03.java
+++ src/main/java/B_Condicionales/Practica03.java
@@ -1,14 +1,28 @@
 /*  Practica 03:
-    Hacer un programa que lea un carácter por teclado compruebe si es una letra mayúscula.
+    Hacer un programa que lea un caracter por teclado compruebe si es una letra mayúscula.
  */
 package B_Condicionales;
 
-/**ç
- * @author Ahmed
+import java.util.Scanner;
+
+/**
+ * @author Ahmed Darhbane
  */
 public class Practica03 {
+
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        char letra;
+
+        System.out.print("Digite una letra: ");
+        letra = sc.next().charAt(0);
+
+        if (Character.isUpperCase(letra)) {
+            System.out.println("La letra esta en MAYUSCULA");
+        } else {
+            System.out.println("La letra esta en MINUSCULA");
+        }
+
     }
-    
+
 }
---
+++ src/main/java/B_Condicionales/Practica05.java
@@ -0,0 +1,15 @@
+/*  Practica06: 
+    Un obrero necesita calcular su salario semanal, el cual se obtiene de la siguiente manera:
+        - Si trabaja 40 horas o menos se le paga $16 por hora
+        - Si trabaja mas de 40 horas se le paga $16 por cada una de las primeras 40 horas y $20 por cada hora extra.
+ */
+package B_Condicionales;
+
+/**
+ * @author Ahmed Darhbane
+ */
+public class Practica05 {
+    public static void main(String[] args) {
+        
+    }
+}
