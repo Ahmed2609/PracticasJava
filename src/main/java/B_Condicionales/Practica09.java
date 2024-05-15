f777643923b68b2294a8033e87b6b79bf23dc489
DAWTarde <DAWTarde@AEMCENT-PN6GCRF>
2024-05-15 17:04:21 +0200

    subiendo practicas

---
+++ src/main/java/B_Condicionales/Practica09.java
@@ -0,0 +1,20 @@
+/*  Practica 10:
+    Pedir el día, mes yaño de una fecha e indicar si la fecha es correcta. 
+    Suponiendo que todos los meses son de 30 días.
+ */
+package B_Condicionales;
+
+import java.util.Scanner;
+
+/**
+ * @author Ahmed Darhbane
+ */
+public class Practica09 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        
+
+    }
+}
