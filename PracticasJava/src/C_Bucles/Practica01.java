e1d3eaa8f2af4f903ca1312d4a266020e53ac3ee
Ahmed PC <darhbaneahmed@gmail.com>
2024-05-15 20:00:16 +0200

    Subiendo practicas

--- PracticasJava/src/C_Bucles/Practica01.java
+++ PracticasJava/src/C_Bucles/Practica01.java
@@ -1,13 +1,33 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package C_Bucles;
 
+/*  Practica 01:
+    Leer un número y mostrar su cuadrado, repetir el proceso hasta que se introduzca un número negativo.
+ */
+
+
+import java.util.Scanner;
+
 /**
- *
- * @author DAW
+ * @author Ahmed Darhbane
  */
 public class Practica01 {
-    
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numero, cuadrado;
+
+        while (true) {
+            System.out.print("Ingrese un numero (numero negativo para salir): ");
+            numero = sc.nextInt();
+
+            if (numero < 0) {
+                System.out.println("Ha ingresado un numero negativo. Saliendo...");
+                break;
+            }
+
+            cuadrado = numero * numero;
+            System.out.println("El cuadrado de " + numero + " es: " + cuadrado);
+        }
+
+    }
 }
---
+++ PracticasJava/src/C_Bucles/Practica02.java
@@ -0,0 +1,22 @@
+package C_Bucles;
+
+/*  Practica 02:
+    Leer un número e indicar si es positivo o negativo. EI proceso se repetirá hasta que se introduzca un 0.
+ */
+
+
+import java.util.Scanner;
+
+/**
+ * @author Ahmed Darhbane
+ */
+public class Practica02 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+
+        
+
+    }
+}
