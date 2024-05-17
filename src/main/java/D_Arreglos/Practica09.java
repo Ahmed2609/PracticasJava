b862242a93d78762a45e4e6baefa5e3ad76d4cec
Ahmed Dar <darhbaneahmed@gmail.com>
2024-05-18 00:15:55 +0200

    Subiendo practicas

---
+++ src/main/java/D_Arreglos/Practica09.java
@@ -0,0 +1,41 @@
+/*  Practica 09: 
+    Crear un programa que lea por teclado una tabla de 10 números enteros y la desplace una posición hacia abajo: 
+    el primero pasa a ser el segundo, el segundo pasa a ser el tercero y así sucesivamente. 
+    EI último pasa a ser el primero.
+*/
+package D_Arreglos;
+
+import java.util.Scanner;
+
+/**
+ * @author Ahmed Darhbane
+ */
+public class Practica09 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] numeros = new int[10];
+        
+        
+        System.out.println("Ingrese los numeros en la .:Tabla:.");
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.print("Digite un numero " + (i + 1) + "/10: ");
+            numeros[i] = sc.nextInt();
+        }
+        
+        int ultimo = numeros[9];
+        
+        for (int i = 8; i >= 0; i--) {
+            numeros[i+1] = numeros[i];
+        }
+        
+        numeros[0] = ultimo;
+        
+        for(int numero:numeros){
+            System.out.print(numero+" ");
+        }
+        
+        
+    }
+    
+}
