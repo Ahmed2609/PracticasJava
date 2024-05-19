c031e829e3cfbab27080b9e02a0820c343e1abdc
Ahmed Dar <darhbaneahmed@gmail.com>
2024-05-19 12:05:07 +0200

    Subiendo practicas poo

--- src/main/java/POO/Practica02/Main.java
+++ src/main/java/POO/Practica02/Main.java
@@ -1,6 +1,11 @@
-/*
+/*  Practica 02: 
+    Construir un programa que permita dirigir el movimiento de un objeto dentro de un tablero 
+    y actualice su posición dentro del mismo. Los movimientos posibles son ARRIBA, ABAJO, IZQUIERDA y DERECHA. 
+    Tras cada movimiento el programa mostrará la nueva dirección elegida y las coordenadas de situación del objeto dentro del tablero.
  */
 package POO.Practica02;
+
+import java.util.Scanner;
 
 /**
  * @author Ahmed Darhbane
@@ -8,6 +13,44 @@ package POO.Practica02;
 public class Main {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Tablero tab1;
+        int x, y, opcion, incremento=0;
+
+        System.out.print("Digite la coordenada inicial de X: : ");
+        x = sc.nextInt();
+        System.out.print("Digite la coordenada inicial de Y: ");
+        y = sc.nextInt();
+
+        tab1 = new Tablero(x, y);
+
+        do {
+            System.out.println("\n\t:.MENU:. ");
+            System.out.println("1. Mover hacia ARRIBA");
+            System.out.println("2. Mover hacia ABAJO");
+            System.out.println("3. Mover hacia la DERECHA");
+            System.out.println("4. Mover hacia la IZQUIERDA");
+            System.out.println("5. Salir");
+            System.out.print("Digite la opcion: ");
+
+            opcion = sc.nextInt();
+
+            if (opcion != 5) {
+                System.out.print("Digite la cantidad de espacios a moverse: ");
+                incremento = sc.nextInt();
+            }
+
+            switch (opcion) {
+                case 1: tab1.moverArriba(incremento);break;
+                case 2: tab1.moverAbajo(incremento);break;
+                case 3: tab1.moverDerecha(incremento);break;
+                case 4: tab1.moverIzquierda(incremento);break;
+                case 5: break;
+                default: System.out.println("Error!! se equivoco de opcion de menu");break;
+            }
+            
+            System.out.println("\nPosicion Actual(X,Y): (" + tab1.getX() + "," + tab1.getY() + ")");
+        } while (opcion != 5);
 
     }
 }
---
+++ src/main/java/POO/Practica03/Main.java
@@ -0,0 +1,16 @@
+/*  Practica 03: 
+    Construir un programa que dada serie de vehículos caracterizados por su marca, modelo y precio,
+    imprima las propiedades del vehículo más barato. Para ello, se deberán leer por teclado 
+    las características de cada vehículo y crear una clase que represente a cada uno de ellos.
+ */
+package POO.Practica03;
+
+/**
+ * @author Ahmed Darhbane
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
