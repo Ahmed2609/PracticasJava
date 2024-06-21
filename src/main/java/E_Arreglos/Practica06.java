47eda18ca461c204c6ac3b6e1d0ba24e677fbb1e
Ahmed Darhbane <darhbaneahmed@gmail.com>
2024-06-21 20:30:47 +0200

    Subiendo practica de arreglos

--- src/main/java/E_Arreglos/Practica06.java
+++ src/main/java/E_Arreglos/Practica06.java
@@ -1,6 +1,6 @@
-/*  Practica 05:
-    Leer por teclado dos tablas de 10 números enteros y mezclarlas en una 
-    tercera de la forma: el 1º de A, el 1º de B, el 2º de A, el 2º de B, etc.
+/*  Practica 06:
+    Leer los datos correspondiente a dos tablas de 12 elementos numéricos, y mezclarlos en una, 
+    tercera de la forma: 3 de la tabla A, 3 de la B, otros 3 de A, otros 3 de la B, etc.
 */
 package E_Arreglos;
 
@@ -9,12 +9,12 @@ import java.util.Scanner;
 /**
  * @author Ahmed Darhbane
  */
-public class Practica05 {
+public class Practica06 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] a = new int[10];
-        int[] b = new int[10];
-        int[] c = new int[20];
+        int[] a = new int[12];
+        int[] b = new int[12];
+        int[] c = new int[24];
         
         // Leer primer arreglo A    
         System.out.println("\nDigite el PRIMER arreglo:");
@@ -30,22 +30,26 @@ public class Practica05 {
             b[i] = sc.nextInt();
         }
         
-        // Mezclar arreglos en C
+        int i=0;
         int j=0;
-        int k=0;
-        for (int i=0; i<c.length; i+=2) {
-            c[i] = a[j];
-            c[i+1] = b[k];
-            j++;
-            k++;
+        while(i<12){
+            for (int k=0; k<3; k++) {
+                c[j] = a[i+k];
+                j++;
+            }
+            
+            for (int k=0; k<3; k++) {
+                c[j] = b[i+k];
+                j++;
+            }
+            i+=3;
         }
         
         // Imprimir el arreglo mezclado C
         System.out.println("\nEl Tercer arreglo es: ");
-        for (int i=0; i<c.length; i++) {
-            System.out.print(c[i] +" ");
+        for (int k=0; k<c.length; k++) {
+            System.out.print(c[k] +" ");
         }
         System.out.println();
-        
     }
 }
---
+++ src/main/java/E_Arreglos/Practica07.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package E_Arreglos;
+
+/**
+ *
+ * @author Ahmed
+ */
+public class Practica07 {
+    public static void main(String[] args) {
+       
+    }
+}
